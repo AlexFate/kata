c5f13b4d1a9f29312e52acf5c51b6bd2d9de4d67
Александр Понизов <aleksandrponizov@gmail.com>
2020-08-25 14:48:13 +0900

    Add answer on LastDigitOfLarge kata, but no c# in it. P.S. rewrite code on Python3

---
+++ CodeWars/Katas/LastDigitOfLarge/Kata.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace CodeWars
+{
+    public partial class Kata
+    {
+        public static int LastDigit(string a, string b)
+        {
+            if (IsNumberZero(b) || IsNumberZero(a) && IsNumberZero(b)) return 1; 
+
+            if (IsNumberZero(a)) return 0;
+            
+            var remainder = GetOnFourRemainder(b);
+            
+            var exp = remainder == 0 ? 4 : remainder; 
+            
+            var result = (int)Math.Pow(a[^1] - '0', exp); 
+            
+            return result % 10; 
+        }
+        private static int GetOnFourRemainder(IEnumerable<char> b)
+        {
+            var result = 0;
+            foreach (var c in b) 
+                result = (result * 10 + (c - '0')) % 4;
+            return result;
+        }
+
+        private static bool IsNumberZero(string input) => input.Length == 1 && input[0] == '0';
+    }
+}
---
+++ Sample_Test/LastDigitOfLargeTest.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using CodeWars;
+using Xunit;
+
+namespace Sample_Test
+{
+    public class LastDigitOfLargeTest
+    {
+        [Theory]
+        [InlineData("3715290469715693021198967285016729344580685479654510946723", "68819615221552997273737174557165657483427362207517952651", 7)]
+        [InlineData("150", "53", 0)]
+        [InlineData("4", "1", 4)]
+        [InlineData("9", "7", 9)]
+        [InlineData("10", "100", 0)]
+        public void Test(string a, string b, int expected)
+        {
+            Assert.Equal(expected, Kata.LastDigit(a, b));
+        }
+    }
+}
