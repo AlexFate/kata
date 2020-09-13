2391827e8c8827f8957e2950fb29ed7518be0e08
Aleksandr Ponizov <aleksandrponizov@gmail.com>
2020-09-13 18:58:42 +0900

    AssemblyAdvanced work, but msg instruction not.

--- CodeWars/Katas/AssemblyAdvanced/Kata.cs
+++ CodeWars/Katas/AssemblyAdvanced/Kata.cs
@@ -7,18 +7,26 @@ namespace CodeWars
 {
     public partial class Kata
     {
-        public static AssemblyAdvanced AssemblyAdvanced => new AssemblyAdvanced();
+        public static string AssemblyAdvancedInterpret(string input) => new AssemblyAdvanced().Interpret(input);
     }
 
-    public sealed class AssemblyAdvanced
+    public class AssemblyAdvanced
     {
-        private static Dictionary<string, int> _variableValue = new Dictionary<string, int>();
-        private static string _output = null;
-        private static int _instructionPointer = 0;
-        private static readonly Stack<int> ReturnPointerStack = new Stack<int>();
-        private static readonly Dictionary<string, int> FunctionPointers = new Dictionary<string, int>();
-        private static Func<int, int, bool> _comparePredicate = null;
-        private static Dictionary<string, Action<string, string>> InstructionActions =>
+        private static class Comparer
+        {
+            public static int A;
+            public static int B;
+            public static bool InvokeComparison(Func<int, int, bool> predicate)
+            {
+                return predicate.Invoke(A, B);
+            }
+        }
+        private readonly Dictionary<string, int> _variableValue = new Dictionary<string, int>();
+        private string _output = null;
+        private int _instructionPointer = 0;
+        private readonly Stack<int> _returnPointerStack = new Stack<int>();
+        private readonly Dictionary<string, int> _functionPointers = new Dictionary<string, int>();
+        private Dictionary<string, Action<string, string>> InstructionActions =>
             new Dictionary<string, Action<string, string>>
             {
                 ["mov"] = Move,
@@ -32,43 +40,84 @@ namespace CodeWars
                 ["ret"] = Return,
                 ["jmp"] = Jump,
                 ["msg"] = Message,
-                ["cmp"] = Compare, 
+                ["cmp"] = Compare,
+                ["jne"] = JumpNotEqual,
+                ["je"] = JumpEqual,
+                ["jge"] = JumpGreaterOrEqual,
+                ["jg"] = JumpGreater,
+                ["jle"] = JumpLessOrEqual,
+                ["jl"] = JumpLess,
                 ["end"] = End
             };
 
-        private static void Compare(string arg1, string arg2)
+        private void JumpLess(string arg1, string arg2)
         {
-            if(_comparePredicate == null) return;
-            else
-            {
-                var isInt = int.TryParse(arg2, out var intValue);
-                if (!_variableValue.ContainsKey(arg1))
-                    _variableValue.Add(arg1, isInt ? intValue : _variableValue[arg2]);
-                else _variableValue[arg1] = isInt ? intValue : _variableValue[arg2];
-            }
+            if (!Comparer.InvokeComparison((i, i1) => i < i1))
+                return;
+            Jump(arg1, null);
         }
-        private static void End(string arg1, string arg2) => _instructionPointer = int.MaxValue - 2;
-        private static void Jump(string arg1, string arg2) => _instructionPointer = FunctionPointers[arg1];
-        private static void Return(string arg1, string arg2) => _instructionPointer = ReturnPointerStack.Pop(); 
-        private static void Call(string arg1, string arg2)
+
+        private void JumpLessOrEqual(string arg1, string arg2)
         {
-            ReturnPointerStack.Push(_instructionPointer);
-            _instructionPointer = FunctionPointers[arg1];
+            if (!Comparer.InvokeComparison((i, i1) => i == i1 || i < i1))
+                return;
+            Jump(arg1, null);
         }
-        private static void Message(string arg1, string arg2)
-            => _output = arg1 + string.Join(" ", arg2.Split(' ').Select(item =>
-                _variableValue.ContainsKey(item) ? _variableValue[item].ToString() : item).Where(item => !string.IsNullOrEmpty(item)));
-        private static void Add(string arg1, string arg2)
+
+        private void JumpGreater(string arg1, string arg2)
+        {
+            if (!Comparer.InvokeComparison((i, i1) => i > i1))
+                return;
+            Jump(arg1, null);
+        }
+
+        private void JumpGreaterOrEqual(string arg1, string arg2)
+        {
+            if (!Comparer.InvokeComparison((i, i1) => i == i1 || i > i1))
+                return;
+            Jump(arg1, null);
+        }
+        private void JumpEqual(string arg1, string arg2)
+        {
+            if (!Comparer.InvokeComparison((i, i1) => i == i1))
+                return;
+            Jump(arg1, null);
+        }
+        private void JumpNotEqual(string arg1, string arg2)
+        {
+            if (!Comparer.InvokeComparison((i, i1) => i != i1))
+                return;
+            Jump(arg1, null);
+        }
+        private void Compare(string arg1, string arg2)
+        {
+            Comparer.A = _variableValue[arg1];
+            Comparer.B = int.TryParse(arg2, out var intValue) ? intValue : _variableValue[arg2];
+        }
+        private void End(string arg1, string arg2) => _instructionPointer = int.MaxValue - 2;
+        private void Jump(string arg1, string arg2) => _instructionPointer = _functionPointers[arg1];
+        private void Return(string arg1, string arg2) => _instructionPointer = _returnPointerStack.Pop(); 
+        private void Call(string arg1, string arg2)
+        {
+            _returnPointerStack.Push(_instructionPointer);
+            _instructionPointer = _functionPointers[arg1];
+        }
+        private void Message(string arg1, string arg2)
+        {
+            // TODO: Something should be here
+            //_output += string.Join("", outputArgs);
+        }
+        private void Add(string arg1, string arg2)
             => _variableValue[arg1] += _variableValue.ContainsKey(arg2) ? _variableValue[arg2] : int.Parse(arg2);
-        private static void Subtract(string arg1, string arg2)
+        private void Subtract(string arg1, string arg2)
             => _variableValue[arg1] -= _variableValue.ContainsKey(arg2) ? _variableValue[arg2] : int.Parse(arg2);
-        private static void Multiply(string arg1, string arg2)
+        private void Multiply(string arg1, string arg2)
             => _variableValue[arg1] *= _variableValue.ContainsKey(arg2) ? _variableValue[arg2] : int.Parse(arg2);
-        private static void Divide(string arg1, string arg2)
+        private void Divide(string arg1, string arg2)
             => _variableValue[arg1] /= _variableValue.ContainsKey(arg2) ? _variableValue[arg2] : int.Parse(arg2);
-        private static void Decrease(string arg1, string arg2 = null) => _variableValue[arg1]--;
-        private static void Increase(string arg1, string arg2 = null) => _variableValue[arg1]++;
-        private static void Move(string nameA, string nameB)
+        private void Decrease(string arg1, string arg2 = null) => _variableValue[arg1]--;
+        private void Increase(string arg1, string arg2 = null) => _variableValue[arg1]++;
+        private void Move(string nameA, string nameB)
         {
             var isInt = int.TryParse(nameB, out var intValue);
             if (!_variableValue.ContainsKey(nameA))
@@ -76,9 +125,8 @@ namespace CodeWars
             else _variableValue[nameA] = isInt ? intValue : _variableValue[nameB];
         }
 
-        public Dictionary<string, int> Interpret(string program)
+        public string Interpret(string program)
         {
-            ClearRegistry();
             _output = null;
             var lines = 
                 program.Split(Environment.NewLine)
@@ -86,37 +134,37 @@ namespace CodeWars
                     .Select(item => !item.Contains(";") ? item : string.Join("", item.TakeWhile(ch => ch != ';')))
                     .Where(item => !string.IsNullOrEmpty(item))
                     .ToImmutableList();
-            lines.Where(str => str.EndsWith(':')).ToImmutableList().ForEach(item => FunctionPointers.Add(item.Replace(":",""), lines.IndexOf(item)));
-            return Interpret(lines);
+            lines.Where(str => str.EndsWith(':')).ToImmutableList().ForEach(item => _functionPointers.Add(item.Replace(":",""), lines.IndexOf(item)));
+            Interpret(lines);
+            return _output;
         }
 
-        public string GetOutput() => _output;
-        
-        private static Dictionary<string, int> Interpret(IReadOnlyList<string> program)
+        private void Interpret(IReadOnlyList<string> program)
         {
             while (_instructionPointer < program.Count)
             {
                 ProcessInstruction(program[_instructionPointer]);
                 _instructionPointer++;
             }
-            var result = _variableValue;
-            ClearRegistry();
-            return result;
         }
 
-        private static void ProcessInstruction(string input)
+        private void ProcessInstruction(string input)
         {
+            if (input.Contains("msg"))
+            {
+                input = SkipStartEmpties(input);
+                var content = input.Substring(3, input.Length - 3);
+                InstructionActions["msg"].Invoke(content, null);
+                return;
+            }
             var parts = input
-                .Split(input.Contains("msg") ? "'" : " ")
+                .Split(" ")
                 .Select(item => item.Replace(",", ""))
                 .Where(item => !string.IsNullOrEmpty(item))
                 .ToImmutableList();
-            InstructionActions[parts[0].Replace(" ", "")].Invoke(parts.Count == 1 ? null : parts[1], parts.Count < 3 ? null : parts[2]);
+            InstructionActions[parts[0]].Invoke(parts.Count == 1 ? null : parts[1], parts.Count < 3 ? null : parts[2]);
         }
-        private static void ClearRegistry()
-        {
-            _variableValue = new Dictionary<string, int>();
-            _instructionPointer = 0;
-        }
+        private static string SkipStartEmpties(string item) =>
+            string.Join("", item.SkipWhile(ch => ch == ' '));
     }
 }
--- Sample_Test/AssemblyAdvancedTest.cs
+++ Sample_Test/AssemblyAdvancedTest.cs
@@ -1,30 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using CodeWars;
+
 using Xunit;
+
 namespace Sample_Test
 {
     public class AssemblyAdvancedTest
     {
         [Theory]
         [ClassData(typeof(CommandTestDataGenerator))]
-        public void ProgramExecutionTests(string commands, Dictionary<string, int> expectedMem, string expectedOutput)
+        public void ProgramExecutionTests(string program, Dictionary<string, int> expectedMem, string expectedOutput)
         {
-            Assert.Equal(expectedMem, Kata.AssemblyAdvanced.Interpret(commands));
-            Assert.Equal(expectedOutput, Kata.AssemblyAdvanced.GetOutput());
+            Assert.Equal(expectedOutput, Kata.AssemblyAdvancedInterpret(program));
         }
     }
     
     public class CommandTestDataGenerator : IEnumerable<object[]>
     {
+        private const string BigProgram1 =
+            "\nmov   a, 8            ; value\nmov   b, 0            ; next\nmov   c, 0            ; counter\nmov   d, 0            ; first\nmov   e, 1            ; second\ncall  proc_fib\ncall  print\nend\n\nproc_fib:\n    cmp   c, 2\n    jl    func_0\n    mov   b, d\n    add   b, e\n    mov   d, e\n    mov   e, b\n    inc   c\n    cmp   c, a\n    jle   proc_fib\n    ret\n\nfunc_0:\n    mov   b, c\n    inc   c\n    jmp   proc_fib\n\nprint:\n    msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text\n    ret\n";
         private readonly List<object[]> _data = new List<object[]>
         {
             new object[] {"mov a, 5", new Dictionary<string, int> {["a"] = 5}, null},
+            new object[] {"msg   'mod(', 5, ', ', 6, ') = ', 7        ; output", null, "mod(5, 6) = 7"},
             new object[] {"mov a, 5\nmov b, a", new Dictionary<string, int> {["a"] = 5, ["b"] = 5}, null},
             new object[] {"mov a, 5\ninc a\ndec a\ndec a", new Dictionary<string, int> {["a"] = 4}, null},
             new object[] {"mov a, 3\nmsg '(5+1)/2 = ', a   ; output message\n", new Dictionary<string, int> {["a"] = 3}, "(5+1)/2 = 3"},
             new object[] {"mov a, 3\ncall f1\ndec a\nend\nf1:\ndec a\nmul a, 2\nret", new Dictionary<string, int> {["a"] = 3}, null},
-            new object[] {"\nmov   a, 5\nmov   b, a\nmov   c, a\ncall  proc_fact\ncall  print\nend\n\nproc_fact:\n    dec   b\n    mul   c, b\n    cmp   b, 1\n    jne   proc_fact\n    ret\n\nprint:\n    msg   a, '! = ', c ; output text\n    ret\n", new Dictionary<string, int> {["a"] = 5, ["b"] = 1, ["c"] = 120}, "5! = 120"}
+            new object[] {"\nmov   a, 5\nmov   b, a\nmov   c, a\ncall  proc_fact\ncall  print\nend\n\nproc_fact:\n    dec   b\n    mul   c, b\n    cmp   b, 1\n    jne   proc_fact\n    ret\n\nprint:\n    msg   a, '! = ', c ; output text\n    ret\n", new Dictionary<string, int> {["a"] = 5, ["b"] = 1, ["c"] = 120}, "5! = 120"},
+            new object[] {BigProgram1, new Dictionary<string, int> {["a"] = 5, ["b"] = 1, ["c"] = 120}, "Term 8 of Fibonacci series is: 21"}
         };
 
         public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
