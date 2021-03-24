a80a30eabf5bbb835e796895e2ab44c0a00b137b
Aleksandr Ponizov <aleksandrponizov@gmail.com>
2021-03-24 10:26:44 +0900

    The lift kata complete

---
+++ CodeWars/Katas/TheLift/Kata.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public partial class Kata
+    {
+        public static int[] TheLift(int[][] floorsAndPeoples, int capacity)
+        {
+            var lift = new Lift(capacity);
+            var controlPanel = new ControlPanel(lift, floorsAndPeoples);
+            var memory = controlPanel.WorkedUp();
+            
+            return memory.ToArray().Reverse().ToArray();
+        }
+
+        public sealed class Lift
+        {
+            private List<int> Slots { get; }
+            private int MaxCapacity { get; }
+            private int FreeSlots => MaxCapacity - Slots.Count;
+
+            public Lift(int maxCapacity)
+            {
+                Slots = new List<int>(maxCapacity);
+                MaxCapacity = maxCapacity;
+            }
+            
+            /// <summary>
+            /// Push people into the lift. It looked on free slots in the lift.
+            /// Returns the people who continue awaiting on the floor
+            /// </summary>
+            /// <param name="waitingPeople"></param>
+            public IEnumerable<int> PushPeople(IEnumerable<int> waitingPeople)
+            {
+                var requiredSlots = waitingPeople.Count();
+                if (requiredSlots <= FreeSlots)
+                {
+                    Slots.AddRange(waitingPeople);
+                    waitingPeople = Array.Empty<int>();
+                }
+                else if (FreeSlots != 0)
+                {
+                    var takeSkipValue = FreeSlots;
+                    Slots.AddRange(waitingPeople.Take(takeSkipValue));
+                    waitingPeople = waitingPeople.Skip(takeSkipValue);
+                }
+                return waitingPeople;
+            }
+
+            /// <summary>
+            /// Is lift should stop on the floor
+            /// </summary>
+            /// <param name="currentState"></param>
+            /// <returns></returns>
+            public bool IsStopRequired(int currentState)
+            {
+                return Slots.Contains(currentState);
+            }
+
+            /// <summary>
+            /// Pop people from lift to floor
+            /// </summary>
+            /// <param name="currentState"></param>
+            public void PopPeople(int currentState)
+            {
+                Slots.RemoveAll(item => item == currentState);
+            }
+        }
+        public sealed class ControlPanel
+        {
+            private readonly Lift _lift;
+            private readonly int[][] _floorsWithQueues;
+            private readonly int _maxFloor;
+            private readonly int _minFloor;
+            private Stack<int> VisitsMemory { get; }
+            private int CurrentState { get; set; }
+            private int StateChanger { get; set; }
+            private Func<int, bool> IsLiftAwaitedFunc { get; set; }
+            private Func<bool> IsMovingShouldContinueFunc { get; set; }
+
+            public ControlPanel(Lift lift, int[][] floorsWithQueues)
+            {
+                _lift = lift;
+                _floorsWithQueues = floorsWithQueues;
+                _maxFloor = floorsWithQueues.Length - 1;
+                VisitsMemory = new Stack<int>();
+                _minFloor = 0;
+                CurrentState = _minFloor;
+                VisitsMemory.Push(CurrentState);
+            }
+
+            /// <summary>
+            /// Lift working process
+            /// </summary>
+            /// <returns></returns>
+            public Stack<int> WorkedUp()
+            {
+                while (_floorsWithQueues.Any(item => item.Any()))
+                {
+                    if (CurrentState == _minFloor)
+                    {
+                        SetMovingUpRules();
+                    }
+                    if (CurrentState == _maxFloor)
+                    {
+                        SetMovingDownRules();
+                    }
+                    MoveTheLift();
+                }
+
+                MoveLiftOnGround();
+                return VisitsMemory;
+            }
+
+            private void MoveLiftOnGround()
+            {
+                if (VisitsMemory.Peek() == _minFloor) return;
+                
+                VisitsMemory.Push(_minFloor);
+                CurrentState = _minFloor;
+            }
+
+            /// <summary>
+            /// Lift movement logic
+            /// </summary>
+            private void MoveTheLift()
+            {
+                while (IsMovingShouldContinueFunc())
+                {
+                    ref var floorQueue = ref _floorsWithQueues[CurrentState];
+                    if (_lift.IsStopRequired(CurrentState))
+                    {
+                        VisitsMemory.Push(CurrentState);
+                        _lift.PopPeople(CurrentState);
+                    }
+                    if (IsSomebodyAwaitTheLift(floorQueue))
+                    {
+                        if(VisitsMemory.Peek() != CurrentState)
+                            VisitsMemory.Push(CurrentState);
+                    
+                        var notMigratedPeople = _lift.PushPeople(GetPotentialPassengers(floorQueue)).ToList();
+                        notMigratedPeople.AddRange(GetNextDoorGuys(floorQueue));
+                        floorQueue = notMigratedPeople.ToArray();
+                    }
+                    CurrentState += StateChanger;
+                }
+                CurrentState -= StateChanger;
+            }
+            
+            private bool IsSomebodyAwaitTheLift(IEnumerable<int> floorQueue)
+            {
+                return floorQueue.Any(IsLiftAwaitedFunc);
+            }
+            
+            private IEnumerable<int> GetPotentialPassengers(IEnumerable<int> floor)
+            {
+                return floor.Where(IsLiftAwaitedFunc);
+            }
+
+            private IEnumerable<int> GetNextDoorGuys(IEnumerable<int> floor)
+            {
+                return floor.Where(item => !IsLiftAwaitedFunc(item));
+            }
+
+            private void SetMovingUpRules()
+            {
+                IsLiftAwaitedFunc = item => item > CurrentState;
+                IsMovingShouldContinueFunc = () => CurrentState <= _maxFloor;
+                StateChanger = 1;
+            }
+            
+            private void SetMovingDownRules()
+            {
+                IsLiftAwaitedFunc = item => item < CurrentState;
+                IsMovingShouldContinueFunc = () => CurrentState >= _minFloor;
+                StateChanger = -1;
+            }
+        }
+    }
+}
---
+++ Sample_Test/TheLiftTest.cs
@@ -0,0 +1,162 @@
+﻿using Xunit;
+using System;
+using CodeWars;
+
+namespace Sample_Test
+{
+    public sealed class TheLiftTest
+    {
+        [Fact]
+        public void TestMoveUp()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                Array.Empty<int>(), // 1
+                new[] { 5, 5, 5 },  // 2
+                Array.Empty<int>(), // 3
+                Array.Empty<int>(), // 4
+                Array.Empty<int>(), // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(new[] { 0, 2, 5, 0 }, result);
+        }
+
+        [Fact]
+        public void TestMoveUpUp()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                new[] { 3 },        // 1
+                new[] { 4 },        // 2
+                Array.Empty<int>(), // 3
+                new[] { 5 },        // 4
+                Array.Empty<int>(), // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(new[] { 0, 1, 2, 3, 4, 5, 0 }, result);
+        }
+        
+        [Fact]
+        public void TestMoveUpLowCapacity()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                new[] { 3 },        // 1
+                new[] { 5,5,6 },        // 2
+                Array.Empty<int>(), // 3
+                new[] { 5 },        // 4
+                Array.Empty<int>(), // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 2);
+            Assert.Equal(new[] { 0, 1, 2, 3, 4, 5, 2, 5, 6, 0 }, result);
+        }
+        
+        [Fact]
+        public void TestMoveDown()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                Array.Empty<int>(), // 1
+                new[] { 1, 1 },     // 2
+                Array.Empty<int>(), // 3
+                Array.Empty<int>(), // 4
+                Array.Empty<int>(), // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(new[] { 0, 2, 1, 0 }, result);
+        }              
+        
+        [Fact]
+        public void TestMoveDownDown()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                new[] { 0 },        // 1
+                Array.Empty<int>(), // 2
+                Array.Empty<int>(), // 3
+                new[] { 2 },        // 4
+                new[] { 3 },        // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(new[] { 0, 5, 4, 3, 2, 1, 0 }, result);
+        }          
+        
+        [Fact]
+        public void TestMoveDownLowCapacity()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(), // G
+                new[] { 0 },        // 1
+                Array.Empty<int>(), // 2
+                Array.Empty<int>(), // 3
+                new[] { 2, 0 },     // 4
+                new[] { 3, 1 },     // 5
+                Array.Empty<int>(), // 6
+            };
+            var result = Kata.TheLift(queues, 2);
+            Assert.Equal(new[] { 0, 5, 4, 3, 1, 0, 4, 2, 0 }, result);
+        }
+
+        [Fact]
+        public void TestMoveInChangedDirection()
+        {
+            int[][] queues =
+            {
+                new[] { 1 },    // G -> n
+                new[] { 3 },    // 1 -> 1
+                new[] { 3,5,6 },// 2 -> 5,6 -> 
+                new[] { 2, 4 }, // 3 -> 3,3,2 -> 3,3,3
+                new[] { 2, 0 }, // 4 -> 2,0,4 -> 
+                new[] { 3, 1, 6 }, // 5 -> 3,1 -> 1
+                new[] { 0 },    // 6 -> 0,6 -> 6
+            };
+            var result = Kata.TheLift(queues, 2);
+            //Assert.Equal(new[] { 0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0 }, result);
+        }        
+        
+        [Fact]
+        public void TestFireDrill()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(),
+                new [] {0,0,0,0},
+                new [] {0,0,0,0},
+                new [] {0,0,0,0},
+                new [] {0,0,0,0},
+                new [] {0,0,0,0},
+                new [] {0,0,0,0},
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(25, result.Length);
+        }        
+        
+        [Fact]
+        public void TestTrickyQueues()
+        {
+            int[][] queues =
+            {
+                Array.Empty<int>(),     //0
+                new [] {0,0,0,6,},      //1
+                Array.Empty<int>(),     //2
+                Array.Empty<int>(),     //3
+                Array.Empty<int>(),     //4
+                new [] {6,6,0,0,0,6,},  //5
+                Array.Empty<int>(),     //6
+            };
+            var result = Kata.TheLift(queues, 5);
+            Assert.Equal(5, result[4]);
+        }
+    }
+}
