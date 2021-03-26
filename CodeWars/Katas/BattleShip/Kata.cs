d39d50b22c7b21fecf2f45ef78820b422dec879b
Aleksandr Ponizov <aleksandrponizov@gmail.com>
2021-03-26 16:19:53 +0900

    Complete BattleShip kata

---
+++ CodeWars/Katas/BattleShip/Kata.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars
+{
+    public partial class Kata
+    {
+        private const int BiggestShipLength = 4;
+        private const int AllShipsOnFieldPixelSum = 4 + 3 * 2 + 2 * 3 + 1 * 4;
+
+        public static bool ValidateBattlefield(int[,] field)
+        {
+            return IsCorrectCountOfShipPixel(field) && IsCorrectShipCountLength(field) && HasNoShipOnDiagonal(field);
+        }
+
+        private static bool IsCorrectCountOfShipPixel(int[,] field)
+        {
+            return field.Cast<int>().Sum() == AllShipsOnFieldPixelSum;
+        }
+        
+        private static bool IsCorrectShipCountLength(int[,] field)
+        {
+            var list = new List<string>();
+            for (var i = 0; i < field.GetLength(1); i++)
+            {
+                var strings = string.Join("", GetColumn(field, i)).Split('0', StringSplitOptions.RemoveEmptyEntries).ToList();
+                strings.AddRange(string.Join("", GetRow(field, i)).Split('0', StringSplitOptions.RemoveEmptyEntries));
+                
+                if (!strings.All(item => item.Length <= BiggestShipLength)) return false;
+                list.AddRange(strings);
+            }
+            
+            return IsCorrectCountOfAllShips(list.Select(item => item.Length));
+        }
+
+        private static bool IsCorrectCountOfAllShips(IEnumerable<int> ships)
+        {
+            return ships.Count(item => item == 4) <= 1 &&
+                   ships.Count(item => item == 3) <= 2 &&
+                   ships.Count(item => item == 2) <= 3;
+        }
+        
+        private static IEnumerable<T> GetRow<T>(T[,] array, int row)
+        {
+            for (int i = 0; i <= array.GetUpperBound(1); ++i)
+                yield return array[row, i];
+        }
+        
+        private static IEnumerable<T> GetColumn<T>(T[,] array, int column)
+        {
+            for (var i = 0; i <= array.GetUpperBound(0); ++i)
+                yield return array[i, column];
+        }
+
+        private static bool HasNoShipOnDiagonal(int[,] field)
+        {
+            for (int i = 0; i < field.GetLength(0) - 1; i++)
+            {
+                var nextI = i + 1;
+                for (int j = 0; j < field.GetLength(1) - 1; j++)
+                {
+                    var nextJ = j + 1;
+                    if (field[i, j] == 1 && field[i, j] == field[nextI, nextJ] ||
+                        field[nextI, j] == 1 && field[nextI, j] == field[i, nextJ]) 
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Sample_Test/BattleShipTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using CodeWars;
+using Xunit;
+
+namespace Sample_Test
+{
+    public class BattleShipTest
+    {
+        [Fact]
+        public void ValidateCorrectBattlefieldTest()
+        {
+            int[,] field = 
+                {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            
+            Assert.True(Kata.ValidateBattlefield(field));
+        }        
+        
+        [Fact]
+        public void ValidateUncorrectDiagonalBattlefieldTest()
+        {
+            int[,] field = 
+               {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 0, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            
+            Assert.False(Kata.ValidateBattlefield(field));
+        }        
+        
+        [Fact]
+        public void ValidateUncorrectHorizontalShipLengthBattlefieldTest()
+        {
+            int[,] field = 
+               {{1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 1, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            
+            Assert.False(Kata.ValidateBattlefield(field));
+        }
+        
+        [Fact]
+        public void ValidateUncorrectVerticalShipLengthBattlefieldTest()
+        {
+            int[,] field = 
+               {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 0, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            
+            Assert.False(Kata.ValidateBattlefield(field));
+        }
+        
+        [Fact]
+        public void ValidateBattlefieldWithUncorrectShipsCountTest()
+        {
+            int[,] field = 
+            {{1, 0, 0, 0, 0, 1, 1, 1, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            
+            Assert.False(Kata.ValidateBattlefield(field));
+        }
+
+        [Fact]
+        public void ValidateBattlefieldWithUncorrectShipCountTest()
+        {
+            int[,] field =
+            {
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            };
+            
+            Assert.False(Kata.ValidateBattlefield(field));
+        } 
+    }
+}
