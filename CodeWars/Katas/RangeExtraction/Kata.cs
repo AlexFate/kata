9b8804ef2cae626019a364c7db0b86af904c24a8
Aleksandr Ponizov <aleksandrponizov@gmail.com>
2021-09-14 18:33:06 +0300

    Completed RangeExtraction C# kata.

---
+++ CodeWars/Katas/RangeExtraction/Kata.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars 
+{
+    public partial class Kata 
+    {
+        public static string Extract(int[] args)
+        {
+            if (!args.Any()) return "";
+            
+            static bool IsGap(int left, int right) => right - left > 1;
+            var last = args.Length - 1;
+            var start = 0;
+            var ranges = new List<(int, int)>(args.Length);
+            foreach(var current in Enumerable.Range(0, last)) 
+            {
+                var next = current + 1;
+                if (IsGap(args[current], args[next])) 
+                {
+                    ranges.Add((start, current));
+                    start = next;
+                }
+            }
+            ranges.Add((start, last));
+
+            var result = ranges.Select(tup => {
+                var (st, end) = tup;
+                var range = (end - st) switch {
+                    0 => args[end].ToString(),
+                    1 => $"{args[st]},{args[end]}",
+                    _ => $"{args[st]}-{args[end]}"
+                };
+                return range;
+            });
+            return string.Join(',', result);
+        }
+    }
+}
---
+++ Sample_Test/RangeExtractionTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using CodeWars;
+
+namespace Sample_Test
+{
+    public class RangeExtractionTest
+    {
+        [Theory]
+        [InlineData(new[] {-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20}, "-6,-3-1,3-5,7-11,14,15,17-20")]
+        [InlineData(new[] {10, 15}, "10,15")]
+        [InlineData(new[] {10, 11, 12, 13, 15}, "10-13,15")]
+        [InlineData(new int[] {}, "")]
+        public void RangeExtraction(int[] input, string expected)
+        {
+            Assert.Equal(expected, Kata.Extract(input));
+        }
+    }
+}
